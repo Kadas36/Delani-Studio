878b6ba2c5ae4af579f573af6a6ba3bf9cc53355
kadas36 <oduorthomas14@gmail.com>
2020-10-11 15:09:58 +0300

    Refactor function for portfolio icons effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,23 @@
-let icons = ['icon1', 'icon2', 'icon3'];
-
-
-
 $(document).ready(function(){
-        $('.icon1').click(function(){
-            $('.icon1 img').toggle();
-            $('.designDetails').toggle();
-        })
-    
-        $('.icon2').click(function(){
-            $('.icon2 img').toggle();
-            $('.developmentDetails').toggle();
-        })
-    
-        $('.icon3').click(function(){
-            $('.icon3 img').toggle();
-            $('.productDetails').toggle();
-        })
+  let icons = ['.icon1', '.icon2', '.icon3'];
+  icons.forEach(function(icon){
+      console.log(icon)
+      $(icon).click(function(){
+          $(icon + ' img').toggle();
+          $(icon + ' .actions').toggle();
+      })
+  })
 
-        let backgrounds = ['.bg1', '.bg2', '.bg3', '.bg4', '.bg5', '.bg6', '.bg7', '.bg8'];
-        backgrounds.forEach(function(background){
-            console.log(background);
-            $(background).hover(function(){
-                $(background +" .projectName").show();
-                },
-                function(){
-                    $(background+ " .projectName").hide();
-                }
-            );
+  let backgrounds = ['.bg1', '.bg2', '.bg3', '.bg4', '.bg5', '.bg6', '.bg7', '.bg8'];
+  backgrounds.forEach(function(background){
+      $(background).hover(function(){
+          $(background +" .projectName").show();
+          },
+          function(){
+              $(background+ " .projectName").hide();
+          }
+      );
 
-        })
+  })
             
 });
