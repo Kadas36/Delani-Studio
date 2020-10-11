449d61a4a462c3edc7b26b095ce29a8867ae2578
kadas36 <oduorthomas14@gmail.com>
2020-10-12 00:02:00 +0300

    Add mailchimp form

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   let icons = ['.icon1', '.icon2', '.icon3'];
   icons.forEach(function(icon){
-      console.log(icon)
       $(icon).click(function(){
           $(icon + ' img').toggle();
           $(icon + ' .actions').toggle();
